64a8c972be4f947e48c6fb89742cdbf0de4b9bca
BenMillsPerkins <bengmp@gmail.com>
2018-10-23 16:31:54 +0100

    Added folder structure and basic vendor requirements, as well as classes

---
+++ src/AgePartnership/UpdateLTV.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AgePartnership;
+
+/**
+ * AgePartnership\UpdateLTV
+ *
+ * Class for functions for updating the LTV data
+ *
+ * @author Ben Mills Perkins
+ * @copyright Age Partnership Ltd
+ * @license Private
+ *
+ */
+
+class UpdateLTV
+{
+    public function test()
+    {
+        return('test');
+    }
+}
---
+++ tests/AgePartnership/UpdateLTVTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AgePartnership;
+
+use PHPUnit\Framework\TestCase;
+
+class UpdateLTVTest extends TestCase
+{
+    public function test()
+    {
+        print('test');
+    }
+}
